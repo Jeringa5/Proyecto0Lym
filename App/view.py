fc160b81aa2f98fe0c767ab129f73d6e05a9dbb1
Jeringa5 <j.lopezm234@uniandes.edu.co>
2025-02-05 13:21:02 -0500

    main_vista

--- App/view.py
+++ App/view.py
@@ -1,2 +1,17 @@
+import os
+import sys
 import App.logic as logic
 
+def main():
+    """
+    Menu principal
+    """
+    working = True
+    #ciclo del menu
+    while working:
+        inputs = input("Ingresa un string de comandos: ")
+        if int(inputs) == 0:
+            working = False
+            print("Gracias por utilizar el robot") 
+    sys.exit(0)
+    
